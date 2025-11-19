c4db066b64c36066df073b827244e9af7fd71d21
excel-azmin <excel.azmin@gmail.com>
2025-11-19 12:33:18 +0000

    add: pixon system setting docs

---
+++ pix_one/pix_one/doctype/pixone_system_settings/test_pixone_system_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Pixfar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPixOneSystemSettings(FrappeTestCase):
+	pass
